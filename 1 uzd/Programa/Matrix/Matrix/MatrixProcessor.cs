8e9b1771c36ee89d17ae89bdc13cfff9657a6e12
Paulius <paulius.leveris@gmail.com>
2020-09-24 14:24:11 +0300

    Adding content

---
+++ 1 uzd/Programa/Matrix/Matrix/MatrixProcessor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Matrix
+{
+    public class MatrixProcessor
+    {
+        private List<List<int>> Matrix { get; set; }
+
+        public MatrixProcessor(List<List<int>> matrix)
+        {
+            if (IsValidMatrix(matrix))
+            {
+                Matrix = matrix;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid matrix (has to be n x n and n > 2)");
+            }
+        }
+
+        public List<int> GetNegativeNumbersUnderDiagonal()
+        {
+            var negativeNums = new List<int>();
+
+            for (int i = 1; i < Matrix.Count; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    var item = Matrix[i][Matrix.Count - 1 - j];
+
+                    if (item < 0)
+                    {
+                        negativeNums.Add(item);
+                    }
+                }
+            }
+
+            return negativeNums;
+        }
+
+        private bool IsValidMatrix(List<List<int>> matrix)
+        {
+            var matrixCount = matrix.Count;
+
+            if (matrixCount <= 2)
+            {
+                return false;
+            }
+
+            foreach (var row in matrix)
+            {
+                if (row.Count != matrix.Count)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 1 uzd/Programa/Matrix/Matrix/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrix = new List<List<int>>();
+            var l1 = new List<int> { 1, 2, 3, 4 };
+            var l2 = new List<int> { 3, 5, 5, 6 };
+            var l3 = new List<int> { -1, -2, 3, 7 };
+            var l4 = new List<int> { -1, -2, -10, 7 };
+
+            matrix.Add(l1);
+            matrix.Add(l2);
+            matrix.Add(l3);
+            matrix.Add(l4);
+
+            try
+            {
+                var matrixProcessor = new MatrixProcessor(matrix);
+                var negativeNums = matrixProcessor.GetNegativeNumbersUnderDiagonal();
+                OutputResult(negativeNums);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static List<int> GetNegativeNumbers(List<List<int>> matrix)
+        {
+            var negativeNums = new List<int>();
+
+            for (int i = 1; i < matrix.Count; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    var item = matrix[i][matrix.Count - 1 - j];
+
+                    if (item < 0)
+                    {
+                        negativeNums.Add(item);
+                    }
+                }
+            }
+
+            return negativeNums;
+        }
+
+        private static void OutputResult(List<int> negativeNums)
+        {
+            Console.Write("Negative numbers under matrix diagonal: ");
+            foreach (var num in negativeNums)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine($"\nSum: {negativeNums.Sum()}");
+            Console.WriteLine($"Average: {negativeNums.Average()}");
+        }
+
+    }
+}
